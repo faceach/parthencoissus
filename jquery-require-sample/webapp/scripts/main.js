4771988ec0aff3f6cc8cced22f7a04312066a734
Vince Liu <applecompass@gmail.com>
2012-06-26 12:16:56 +0800

    first

---
+++ jquery-require-sample/webapp/scripts/main.js
@@ -0,0 +1,6 @@
+require-jquery.jsrequire(["jquery", "jquery.alpha", "jquery.beta"], function($) {
+    //the jquery.alpha.js and jquery.beta.js plugins have been loaded.
+    $(function() {
+        $('body').alpha().beta();
+    });
+});
