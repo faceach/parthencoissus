a3934c2b02334ba330cabaacc8d7fdacf4848921
faceach <faceach@gmail.com>
2012-06-28 23:35:51 +0800

    add word library

---
+++ web/_data/word.js
@@ -0,0 +1,35 @@
+// word library
+{
+	"A": [
+		{
+			word: "solve",
+			explanation: [
+			    "find the solution to (a problem or question) or understand the meaning of Next"
+			    ]
+		}
+	],
+	"B": [
+		{
+			word: "solve",
+			explanation: [
+			    "find the solution to (a problem or question) or understand the meaning of Next"
+			    ]
+		}
+	],
+	"C": [
+		{
+			word: "solve",
+			explanation: [
+			    "find the solution to (a problem or question) or understand the meaning of Next"
+			    ]
+		}
+	],
+	"D": [
+		{
+			word: "solve",
+			explanation: [
+			    "find the solution to (a problem or question) or understand the meaning of Next"
+			    ]
+		}
+	]
+]
