9f445a63d79b3ba9928bea8db8e05a4bdd91e293
Vince Liu <faceach@gmail.com>
2012-07-03 23:57:26 -0400

    word

--- web/_scripts/prototype/index-main.js
+++ web/_scripts/prototype/index-main.js
@@ -4,12 +4,39 @@ require.config({
     baseUrl: "../_scripts",
     urlArgs: "version=1.0.0",
     paths: {
+        // lib
+        "text": "_lib/require/text",
+        "modernizr": "_lib/require/adapter/modernizr",
+        "mustache": "_lib/require/adapter/mustache",
+
+        "underscore": "_lib/require/adapter/underscore",
+        "backbone": "_lib/require/adapter/backbone",
+        "backbone-localstorage": "_lib/require/adapter/backbone-localstorage",
+
+        "doT": "_lib/doT/doT",
+
+        "jquery.mousewheel": "_lib/jquery/jquery.mousewheel",
+        "jquery.textchange": "_lib/jquery/jquery.textchange",
+
         // common
+        "msglistener": "common/msglistener",
         "preventscrolling": "common/preventscrolling",
 
         // component
-		"roundoff": "component/roundoff",
-        "takeword": "component/takeword"
+        "context": "component/context",
+        "roundoff": "component/roundoff",
+        "takeword": "component/takeword",
+        "getscore": "component/getscore",
+        "updatescore": "component/updatescore",
+        "msghandler": "component/msghandler",
+        "matchpartner": "component/matchpartner",
+        "getwordexplanation": "component/getwordexplanation",
+        "gradehandler": "component/gradehandler"
+    },
+    shim: {
+        'jquery.mousewheel': ['jquery'],
+        'jquery.textchange': ['jquery'],
+        'backbone': ['underscore', 'jquery']
     },
     catchError: true,
     waitSeconds: 7,
--- web/_scripts/widget/waiting/waiting.js
+++ web/_scripts/widget/waiting/waiting.js
@@ -3,39 +3,39 @@
 define(["jquery", "widget/study/study", "text!./template.html", "msghandler", "roundoff"],
 function ($, study, template,MsgHandler, roundoff) {
 
-    var $container = $("#gw-main"),
+	var $container = $("#gw-main"),
 		msgHandler = new MsgHandler;
 
-    function handleSuccess(msg) {
-        console.log("success");
-    };
-    function handleGiveup(msg) {
-        console.log("Give up");
-    };
-    function handleExit(msg) {
-        console.log("Exit");
-    };
-    function handleFail(msg) {
-        console.log("Fail");
-    };
-    function handleMsg() {
-        msgHandler.listen("success", handleSuccess);
-        msgHandler.listen("giveup", handleGiveup);
-        msgHandler.listen("exit", handleExit);
-        msgHandler.listen("fail", handleFail);
-    };
+	function handleSuccess(msg) {
+		console.log("success");
+	};
+	function handleGiveup(msg) {
+		console.log("Give up");
+	};
+	function handleExit(msg) {
+		console.log("Exit");
+	};
+	function handleFail(msg) {
+		console.log("Fail");
+	};
+	function handleMsg() {
+		msgHandler.listen("success", handleSuccess);
+		msgHandler.listen("giveup", handleGiveup);
+		msgHandler.listen("exit", handleExit);
+		msgHandler.listen("fail", handleFail);
+	};
 
-    return function () {
-        var $html = $(template),
-            $study = $html.find(".gw-study");
+	return function () {
+		var $html = $(template),
+			$study = $html.find(".gw-study");
 
-        study($study);
+		study($study);
 
-        $container.empty().append($html);
+		$container.empty().append($html);
 
 		handleMsg();
-        roundoff();
+		roundoff();
 
-    };
+	};
 
 });
