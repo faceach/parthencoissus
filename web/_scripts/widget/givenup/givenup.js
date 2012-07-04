3edebf4b94c2a362c52d2fec90294647258dd18a
faceach <faceach@gmail.com>
2012-07-04 23:38:18 +0800

    handle give up

--- web/_scripts/widget/givenup/givenup.js
+++ web/_scripts/widget/givenup/givenup.js
@@ -1,20 +1,18 @@
 "use strict";
 
-define(["jquery", "widget/takeword/takeword", "widget/word/word", "widget/playbutton/playbutton", "doT","text!./template.html", "roundoff"],
-function ($, takeword, Word, playbutton, doT,template, roundoff) {
+define(["jquery", "widget/takeword/takeword", "widget/word/word", "widget/playbutton/playbutton","doT", "text!./template.html", "roundoff"],
+function ($, takeword, Word, playbutton, doT, template, roundoff) {
 
     var $container = $("#gw-main");
 
-    return function (mistakeWord) {
+    return function () {
         var doTemp = doT.template(template),
 			$html = $(doTemp({"partnerName": takeword.getPartner()})),
 			$word = $html.find(".gw-word"),
-			$mistakeword = $html.find(".gw-mistakeword"),
 			$btnContainer = $html.find(".gw-btn-container");
 
         var word = new Word();
 
-        word.display($mistakeword, mistakeWord, true);
         word.display($word, takeword.getWord());
         playbutton($btnContainer);
 
