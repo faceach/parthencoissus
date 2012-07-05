fd0859c2216e3e14525eacca718069935a0d773f
Izzie Wong <izzie_wang936@hotmail.com>
2012-07-05 00:08:24 -0400

    right

--- web/_scripts/widget/right/right.js
+++ web/_scripts/widget/right/right.js
@@ -1,7 +1,7 @@
 "use strict";
 
-define(["jquery", "doT", "widget/study/study", "text!./template.html", "msghandler", "roundoff"],
-function ($, doT, study, template, MsgHandler, roundoff) {
+define(["jquery", "doT", "text!./template.html", "msghandler", "roundoff"],
+function ($, doT, template, MsgHandler, roundoff) {
 
     var $container = $("#gw-main"),
 		msgHandler = new MsgHandler;
@@ -11,9 +11,6 @@ function ($, doT, study, template, MsgHandler, roundoff) {
             doTemp = doT.template(template);
 
         $container.empty().html(doTemp(data));
-        answer = msg.content.word;
-        btnClickHandler();
-        autoSkip();
         roundoff();
     }
 
