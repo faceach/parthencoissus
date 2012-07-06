29a9e1a03b20b52fe8fb647d23a12eb9dd496293
Vince Liu <faceach@gmail.com>
2012-07-05 22:42:05 -0400

    merge

--- web/_scripts/widget/wordinput/wordinput.js
+++ web/_scripts/widget/wordinput/wordinput.js
@@ -3,68 +3,71 @@
 define(["jquery", "doT", "gethint", "jquery.textchange", "widget/word/word", "text!./template.html", "roundoff"],
 function ($, doT, gethint, textchange, Word, template, roundoff) {
 
-    var word, wordExp, $inputs, $letters;
+	var word, wordExp, $inputs, $letters;
 
-    function keySupport($inputs) {
-        $inputs.bind("textchange", function () {
-            if ($(this).val()) {
-                $(this).next().focus();
-            }
-        });
-    };
-    function getInput($inputs) {
-        var inputWord = "";
-        $inputs.each(function (i, e) {
-            var letter = e.value;
-            if (!letter) {
-                //TODO: warning this input to user
-                //...
-                return false;
-            }
-            inputWord += letter;
-        });
-        return inputWord;
-    };
+	function keySupport($inputs) {
+		$inputs.bind("textchange", function () {
+			if ($(this).val()) {
+				$(this).next().focus();
+			}
+		});
+	};
+	function getInput($inputs) {
+		var inputWord = "";
+		$inputs.each(function (i, e) {
+			var letter = e.value;
+			if (!letter) {
+				//TODO: warning this input to user
+				//...
+				return false;
+			}
+			inputWord += letter;
+		});
+		return inputWord;
+	};
 
-    return {
-        "init": function ($container, originalWord) {
-            if (!originalWord) {
-                return;
-            }
-            wordExp = originalWord;
-            word = originalWord.word;
-            var $html = $(template),
+	return {
+		"init": function ($container, originalWord) {
+			if (!originalWord) {
+				return;
+			}
+			wordExp = originalWord;
+			word = originalWord.word;
+			var $html = $(template),
 				tempGuess = $html.find(".gw-tmp-guess").html(),
 				doTemp = doT.template(tempGuess),
 				$html = $(doTemp(wordExp)),
 				$inputs = $html.find("input.gw-input-letter-valid"),
 				$letters = $html.find("input[type='text']");
 
-            keySupport($inputs);
-            $container.empty().append($html);
-        },
-        "hint": function () {
-            var hintIndex = gethint(word);
-            console.log(hintIndex);
-        },
-        "answer": function () {
-            var $html = $(template),
+			keySupport($inputs);
+			$container.empty().append($html);
+		},
+		"hint": function () {
+			var hintIndex = gethint(word);
+			console.log(hintIndex);
+		},
+		"help": function (explanation) {
+			
+		},
+		"answer": function () {
+			var $html = $(template),
 				tempAnswer = $html.find(".gw-tmp-answer").html(),
 				doTemp = doT.template(tempAnswer),
 				$html = $(doTemp(wordExp)),
 				$word = $html.find(".gw-word");
 
-            var insWord = new Word;
-            insWord.display($word, word, "explanation");
+			var insWord = new Word;
+			insWord.display($word, word, "explanation");
 
-            $container.empty().append($html);
-        },
-        "compare": function () {
-            var guessWord = getInput($letters);
-            return guessWord === word;
-        },
-        "getWord": function () {
-            return word;
-        }
-    };
+			$container.empty().append($html);
+		},
+		"compare": function () {
+			var guessWord = getInput($letters);
+			return guessWord === word;
+		},
+		"getWord": function () {
+			return word;
+		}
+	};
 });
