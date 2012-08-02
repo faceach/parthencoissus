69b520baf80cfae8375c49134aa45ad517e3f29b
Vince Liu <faceach@gmail.com>
2012-08-02 11:31:49 +0800

    unload fix

--- web/public/_scripts/prototype/demo-main.js
+++ web/public/_scripts/prototype/demo-main.js
@@ -24,6 +24,7 @@ require(["jquery"], function ($) {
 				dataType: 'html',
 				success: function (data) {
 					$target.html(data);
+					$this.data("running", true);
 				},
 				error: function () {
 					console.log("error");
