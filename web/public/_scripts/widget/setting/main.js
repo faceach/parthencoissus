81ae83f69783c230091a78d440ad5351c4ee3ca8
Izzie Wong <kungfupanda936@gmail.com>
2013-04-19 11:50:57 +0800

    revised category btns styel

--- web/public/_scripts/widget/setting/main.js
+++ web/public/_scripts/widget/setting/main.js
@@ -13,8 +13,6 @@ function ($, Context, template, roundoff) {
         
         $html.on("click", "[data-action='savesettings']", function(e){
             e.preventDefault();
-            $html.find('a[data-action="savesettings"]').removeClass("gw-category-active");
-            $(this).addClass("gw-category-active");
             var category = $(this).attr("data-category");
             $.extend(Context.get(), {"category": category});
             console.log(Context.get());
